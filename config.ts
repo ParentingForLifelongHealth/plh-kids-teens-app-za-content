bdfacabb3cb63b1a51b0b912f0700e39af72dbfc
EsmeeIDEMS <esmee@idems.international>
2025-02-18 15:46:38 +0000

    update config to refer to folder copies

--- config.ts
+++ config.ts
@@ -11,12 +11,12 @@ config.google_drive.sheets_folder_ids = [
   "19wSspWYMbRc75een-kS0q0aq24--75u8", // library_app_menu
   "1UXVz71HniwdtklFnGUEBzzj8ZHI9oQVo", // library_field_values
   "1Y8uC9-rqQtsjQgUfeX9qp-vNzsFDUQFU", // kids_global
-  "1pzgmUypvXYJS_ZcC1WmiHM5f4y2QYKsx", // kids_kw
+  "1AGh4vVHx1Z9tPTmkYlU-I8LmAuIQ3gVi", // sheets copied from KW
   "1Bzlnwts9mkoLRhDy-SN5O1A3bUlBOynI" // kids_teens_za
 ];
 config.google_drive.assets_folder_ids = [
   "1abaL1QGd33NqqLoKuo2t9fVWKmh5ouM9", // kids_global
-  "1d7Xf_k-kCiQsTGlpSAJnVLTTX5zTVvnp", // kids_kw
+  "1UeLyEG3-PcXHSbS54GQ1B1HwZ8ihbSY1", // assets copied from KW
   "1T93qsaSBbYa-lCF6ChPkfoX85PLugJCZ" // kids_teens_za
 ];
 
